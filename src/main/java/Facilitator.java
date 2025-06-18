a235067a1165221c987fc2d78b3c439908840aec
azu-wakinoza <wakinoza@gmail.com>
2025-06-18 10:55:46 +0900

    Facilitatorクラスの作成し、メソッドを２つ作成
    startGame()とendGame()メソッドを作成

---
+++ src/main/java/Facilitator.java
@@ -0,0 +1,11 @@
+public class Facilitator {
+    public void startGame(){
+        System.out.println("【じゃんけんゲーム】¥n");
+        System.out.println("これより、じゃんけんゲームを開始します");
+        System.out.println("勝負は１回、手はランダムに選ばれます¥n");
+    }
+
+    public void endGame(){
+        System.out.println("¥nじゃんけんゲームを終了します");
+    }
+}
